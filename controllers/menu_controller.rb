039e148641e28ffb3d9a7c9e253fc8a394db07eb
amyworks <yup@amy.works>
2018-09-13 21:42:44 -0400

    Assignment 4 complete

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -11,9 +11,10 @@ require_relative '../models/address_book'
       puts "Main Menu - #{address_book.entries.count} entries"
       puts "1 - View all entries"
       puts "2 - Create an entry"
-      puts "3 - Search for an entry"
-      puts "4 - Import entries from a CSV"
-      puts "5 - Exit"
+      puts "3 - View entry N"
+      puts "4 - Search for an entry"
+      puts "5 - Import entries from a CSV"
+      puts "6 - Exit"
       print "Enter your selection: "
  
       selection = gets.to_i
@@ -29,13 +30,17 @@ require_relative '../models/address_book'
           main_menu
         when 3
           system "clear"
-          search_entries
+          view_entry_n_submenu
           main_menu
         when 4
           system "clear"
-          read_csv
+          search_entries
           main_menu
         when 5
+          system "clear"
+          read_csv
+          main_menu
+        when 6
           puts "Good-bye!"
           exit(0)
         else
@@ -72,6 +77,22 @@ require_relative '../models/address_book'
  
     system "clear"
     puts "New entry created"
+  end
+
+  def view_entry_n_submenu
+    print "Which entry would you like to view?"
+    selection = gets.chomp.to_i
+
+    if selection < @address_book.entries.count
+      puts @address_book.entries[selection]
+      puts "Hit enter to return to Main Menu"
+      gets.chomp
+      system "clear"
+    else
+      puts "#{selection} does not exist."
+      view_entry_n_submenu
+    end
+
   end
  
   def search_entries
